36475a847811110778389453ba003897ab0ec859
johnskorah17 <74665102+johnskorah17@users.noreply.github.com>
2021-11-29 10:37:16 +0530

    Add files via upload

---
+++ johns ds lab cycle1/addtwolist.py
@@ -0,0 +1,8 @@
+NumList1 = [20, 30, 40, 50]
+NumList2 = [45, 25, 35, 45]
+total = []
+
+for j in range(4):
+    total.append(NumList1[j] + NumList2[j])
+
+print("\nThe total Sum of Two Lists =  ", total)
