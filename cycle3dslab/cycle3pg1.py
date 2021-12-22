5197078aff7f011c142ca3094bd3501dcb44f16e
johnskorah17 <74665102+johnskorah17@users.noreply.github.com>
2021-12-22 12:07:00 +0530

    Add files via upload

---
+++ cycle3dslab/cycle3pg1.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+  
+x = [2001, 2002, 2003,2004,2005,2006,2007]
+  
+y = [2400, 22500, 19700,17500, 14500,10000,5800]
+  
+
+plt.plot(x,y)
+  
+plt.title("value depreciation",loc = 'left')
+
+plt.plot(x,y,ls = 'dashdot', marker = '*',mfc = 'green',ms='20',color = "red")
+plt.show()
+
